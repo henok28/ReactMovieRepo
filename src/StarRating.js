27fb8416feec048eca1d0d680e96fae38be5e955
unknown <henokgirma295@gmail.com>
2025-09-23 09:31:05 +0300

    RatingComponenet Hovering Functionality

--- src/StarRating.js
+++ src/StarRating.js
@@ -16,6 +16,7 @@ const textStyle = {
 };
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -27,12 +28,14 @@ export default function StarRating({ maxRating = 5 }) {
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
+            full={tempRating ? tempRating >= i + 1 : rating >= 1 + i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= 1 + i}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -44,10 +47,16 @@ const statStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
-  console.log(full);
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
+  // console.log(full);
   return (
-    <span style={statStyle} role="button" onClick={onRate}>
+    <span
+      style={statStyle}
+      role="button"
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
